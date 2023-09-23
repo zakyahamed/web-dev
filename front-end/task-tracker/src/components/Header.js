d6da4d30f6fbc9cf5700c11ce5dea49b540821a2
Zaky Ahamed <zakynaleem@gmail.com>
2023-09-23 23:15:07 +0530

    first crash course done

--- front-end/task-tracker/src/components/Header.js
+++ front-end/task-tracker/src/components/Header.js
@@ -1,14 +1,19 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("Hello");
-  };
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button onClick={onClick} color="green" text="Hello" />
+      {location.pathname === "/" && (
+        <Button
+          onClick={onAdd}
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+        />
+      )}
     </header>
   );
 };
